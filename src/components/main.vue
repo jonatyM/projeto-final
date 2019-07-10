98bb56df33078360b16f5813fb58a12f2a058fd6
jonaty <jonatypereira@gmail.com>
2019-07-10 20:11:49 -0300

    implemetação da validação

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,47 +1,53 @@
 <template>
     <div id="scta" class="table-secondary">
-        <div id="cadastroLivro" class="form-gruup ">
-            <h2 align="center">{{ cabecalho }}</h2>
-            <label><span class="red-alert">{{ vldLivro }}</span></label>
-            <input v-model="novoLivro" @keyup.enter="addLivro" type="text" placeholder="Titulo do livro..." class="form-control">
-            <br>
-            <label><span class="red-alert">{{ vldAutor }}</span></label>
-            <input v-model="novoAutor" @keyup.enter="addLivro" type="text" placeholder="Autor do livro..." class="form-control">
-            <br/>
-            <span class="input-group-btn">
-                <button @click="addLivro" type="button" class="btn btn-success btn-block">Adicionar</button>
-            </span>
-            <br>
-        </div>
-        <div id="tblLivro">
-            <hr>
-            <ul>
-                <li  class="toggle" v-for="livro of livros" :key="livro" v-bind:class="{ 'removido': livro.checked }">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="list" v-model="livro.checked">
-                            <big>{{ livro.titulo }}</big> - <small>{{ livro.autor }}</small>
-                            <span aligin="right" >
-                                <button class="btn btn-outline-danger" @click="removeLivro(livro)"> x </button>
-                            </span>                            
-                        </label>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <!--<footer class="footer" v-show="todos.length" v-cloak>
-            <span class="todo-count">
-                <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
-            </span>
-            <ul class="filters">
-                <li><a href="#/all" :class="{ selected: visibility == 'all' }">Todos</a></li>
-                <li><a href="#/active" :class="{ selected: visibility == 'active' }">Ativados</a></li>
-                <li><a href="#/completed" :class="{ selected: visibility == 'completed' }">Concluído</a></li>
-            </ul>
-            <button class="clear-completed" @click="removeCompleted" v-show="livros.length > remaining">
-                Clear completed
-            </button>   
-        </footer> -->
+        <form >
+            <div id="cadastroLivro" class="form-gruup ">
+                
+                <h2 align="center">{{ cabecalho }}</h2>
+                <label><span class="red-alert">{{ vldLivro }}</span></label>
+                <input v-model="novoLivro" type="text" placeholder="Titulo do livro..." class="form-control">
+                <br>
+                <label><span class="red-alert">{{ vldAutor }}</span></label>
+                <input v-model="novoAutor" type="text" placeholder="Autor do livro..." class="form-control">
+                <br/>
+                <span class="input-group-btn">
+                    <button @click="addLivro"  type="submit" v-bind:disabled="!isValid" class="btn btn-success btn-block">Adicionar</button>
+                </span>
+                <br>
+            </div>
+
+            <div id="tblLivro">
+                <hr>
+                <ul>
+                    <li  class="toggle" v-for="livro of livros" :key="livro.titulo.autor"  v-bind:class="{ 'removido': livro.checked }">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="list" v-model="livro.checked">
+                                <big>{{ livro.titulo }}</big> - <small>{{ livro.autor }}</small>
+                                <span aligin="right" >
+                                    <button class="btn btn-outline-danger" @click="removeLivro(livro)"> x </button>
+                                </span>                            
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+            
+            </div>
+
+            <!--<footer class="footer" v-show="todos.length" v-cloak>
+                <span class="todo-count">
+                    <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
+                </span>
+                <ul class="filters">
+                    <li><a href="#/all" :class="{ selected: visibility == 'all' }">Todos</a></li>
+                    <li><a href="#/active" :class="{ selected: visibility == 'active' }">Ativados</a></li>
+                    <li><a href="#/completed" :class="{ selected: visibility == 'completed' }">Concluído</a></li>
+                </ul>
+                <button class="clear-completed" @click="removeCompleted" v-show="livros.length > remaining">
+                    Clear completed
+                </button>   
+            </footer> -->
+          </form>
     </div>
 </template>
 
@@ -56,10 +62,7 @@ export default {
             error: [],
             livros: [],
             novoLivro: "",
-            novoAutor: "",
-            //disabledButton: false
-            //visibility: 'all',
-                                    
+            novoAutor: "",                                  
         }
     },
     mounted() {
@@ -71,6 +74,13 @@ export default {
             }
         }
     },
+    computed:{
+        isValid: function(){
+            return this.novoLivro != "" && this.novoAutor != ""
+            && this.novoLivro !== this.novoAutor
+            //&& this.livros.novoLivro.length < 2 && this.livros.novoAutor.length < 4
+        }
+    },
     
     methods: {
         addLivro() {
@@ -80,12 +90,10 @@ export default {
             if (!titulo && autor) {
                 return true
             }
-            //this.validaCampos()
-                //this.disabledButton = true
-                this.livros.push({titulo: titulo, autor: autor, checked: false })
-                this.novoLivro = ""
-                this.novoAutor = ""
-                this.saveLivro();           
+            this.livros.push({titulo: titulo, autor: autor, checked: false })
+            this.novoLivro = ""
+            this.novoAutor = ""
+            this.saveLivro();           
         }, 
         removeLivro(livro){
             this.livros.splice(this.livros.indexOf(livro), 1)
@@ -95,7 +103,8 @@ export default {
             const parsed = JSON.stringify(this.livros);
             localStorage.setItem('livros', parsed);
         },
-        /*validaCampos() {
+    
+       /*validaCampos() {
             var error = 0;
             var titulo, autor,
             titulo = this.novoLivro
@@ -125,7 +134,7 @@ export default {
         ResetError : function(){
             this.error.vldLivro = "*";
             this.error.vldAutor = "*";
-        },*/        
+        }, */       
     },
 }
 </script>
